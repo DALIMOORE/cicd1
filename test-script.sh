f74f06fd67d79269eebf7963b8d2262f31263bb7
DALIMOORE <157513605+DALIMOORE@users.noreply.github.com>
2024-02-14 12:12:16 -0500

    Update test-script.sh

--- test-script.sh
+++ test-script.sh
@@ -1,7 +1,9 @@
 # Simple test script for greetings jar
+
 set -e
-java -jar build/libs/greetings-ci-$1.jar ${@:2} > output.bench
+
+java -jar build/libs/cicd1-$1.jar ${@:2} > output.bench
 IFS=' ' read -ra ARR <<< "${@:2}"
 for i in "${ARR[@]}"; do
-grep "^$i$" output.bench
+  grep "^$i$" output.bench
 done
